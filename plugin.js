9824cfdbb39ea69f95966ff08d50df641b86607f
Shoaib Adnan <shoaibadnan870@gmail.com>
2020-12-07 13:59:29 -0330

    Added main page of the html and modified the list in js file

--- plugin.js
+++ plugin.js
@@ -1,4 +1,4 @@
-let arr = ['HTML', 'CSS', 'JavaScript', 'Java', 'Python', 'SQL'];
+let arr = ['Mountain Bikes', 'Road bikes', 'Hybrid/Commuter bikes', 'Folding bikes', 'Electric bikes', 'Touring bikes'];
 
 $.fn.this = function() {
     
