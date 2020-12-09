4abb0e68e6059887a453554e9f7d4089098fb700
Shoaib Adnan <shoaibadnan870@gmail.com>
2020-12-09 20:26:34 -0330

    Added code to the main javascrpt plugin

--- plugin.js
+++ plugin.js
@@ -1,5 +1,34 @@
 let arr = ['Mountain Bikes', 'Road bikes', 'Hybrid/Commuter bikes', 'Folding bikes', 'Electric bikes', 'Touring bikes'];
 
-$.fn.this = function() {
-    
+$.fn.listThis = function(listType) {
+    let l =  $('<' + listType + '>');
+  
+  this.each(function(i, item) {
+  	$(l).append($('<li>').append(item));
+  });
+ 
+ return l;
 }
+
+$.fn.colorMyList = function(options) {
+	let settings = $.extend({
+  	color: '#000',
+    fontSize: '16px',
+    textdecoration: 'underline'
+  }, options);
+  
+  return this.css({
+  	'color': settings.color,
+    'font-size': settings.fontSize,
+    'text-decoration': settings.textdecoration
+  });
+}
+
+$('#result').append($(arr).listThis('ul').colorMyList(
+	{
+	color: '#0000ff',
+    fontSize: '20px',
+    textDecoration: 'none'
+	}
+))
+;
